3e7631f623c3a9bc3610ddf7c31425a58aa90797
Kemal <kemal.bas.136@gmail.com>
2019-08-03 14:22:24 +0300

    Initial commit

---
+++ src/components/toDo.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Todo  extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Todo ;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+const element = <h1>hello world</h1>;
+
+console.log(element);
